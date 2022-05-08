a7f488eadc97e610706277d40290a633fba19e62
ticha2017 <70824913+ticha2017@users.noreply.github.com>
2022-05-08 16:21:04 -0400

    Add files via upload

---
+++ c1-versions.tf
@@ -0,0 +1,18 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.1"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.3"
+     }
+     null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+     }
+  }
+}
+# Provider Block
+provider "aws" {
+  region = "us-east-1"
+}
